0b51f1ab25c607e3d6fe1ea7ea7d56b86f8fb0b9
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-08-07 11:49:15 +0300

    Update index.js

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+(() => {
+	const refs = {
+		togglerMenu: document.querySelector("[data-menu-button]"),
+		menuItems: document.querySelectorAll("[data-menu-item]"),
+		Menu: document.querySelector("[data-menu-container]")
+	};
+
+	refs.menuItems.forEach(item => {
+		item.addEventListener("click", toggleMenu);
+	});
+	refs.togglerMenu.addEventListener("click", toggleMenu);
+
+	function toggleMenu() {
+		refs.togglerMenu.classList.toggle("is-open");
+		refs.Menu.classList.toggle("is-closed");
+		document.body.classList.toggle("off_overflow");
+	}
+})();
