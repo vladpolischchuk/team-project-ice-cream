bac75e394d9415772edae4e7fbb54cc86ee82f00
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-08-08 21:37:13 +0300

    Create mobile-menu.js

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,26 @@
+(() => {
+	const refs = {
+		togglerMenu: document.querySelector("[data-menu-button]"),
+		menuItems: document.querySelectorAll("[data-menu-item]"),
+		Menu: document.querySelector("[data-menu-container]")
+	};
+
+	refs.menuItems.forEach(item => {
+		item.addEventListener("click", closeMenu);
+	});
+	refs.togglerMenu.addEventListener("click", toggleMenu);
+
+	function toggleMenu() {
+		const expanded = refs.togglerMenu.getAttribute("aria-expanded") === "true" || false;
+		refs.togglerMenu.setAttribute("aria-expanded", !expanded);
+		refs.togglerMenu.classList.toggle("is-open");
+		refs.Menu.classList.toggle("is-closed");
+		document.body.classList.toggle("off_overflow");
+	}
+	function closeMenu() {
+		if (refs.togglerMenu.classList.contains("is-open")){
+			toggleMenu()
+		};
+		
+	}
+})();
