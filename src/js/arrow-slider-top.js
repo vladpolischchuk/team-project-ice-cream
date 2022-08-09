06851ba2aac84b7a2ba11ab88bca077529e3da21
vladpolischchuk <105670228+vladpolischchuk@users.noreply.github.com>
2022-08-09 15:45:17 +0300

    adds button for scroll to header

---
+++ src/js/arrow-slider-top.js
@@ -0,0 +1,13 @@
+function backToTop() {
+    let button = $('.scroll-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+}
+
+backToTop();
---
+++ src/js/menu-slow-scroll.js
@@ -0,0 +1,7 @@
+$(".scroll-to").on("click", function(e){
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top - 0
+    }, 800);
+});
