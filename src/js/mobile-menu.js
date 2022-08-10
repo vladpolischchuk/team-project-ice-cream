9ba0a421c2e8e24ccc2c17b6472b84c580a0ba09
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-08-10 12:14:05 +0300

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,13 +2,21 @@
 	const refs = {
 		togglerMenu: document.querySelector("[data-menu-button]"),
 		menuItems: document.querySelectorAll("[data-menu-item]"),
-		Menu: document.querySelector("[data-menu-container]")
+		Menu: document.querySelector("[data-menu-container]"),
+		openModal: document.querySelector("[data-modal-buy]"),
+		togglerMenu: document.querySelector("[data-menu-button]"),
+		closeModal: document.querySelector("[data-modal-close]"),
+		modal: document.querySelector("[modal-data-buy]"),
+		backDrop: document.querySelector("[data-modal-bg]")
 	};
 
 	refs.menuItems.forEach(item => {
 		item.addEventListener("click", closeMenu);
 	});
 	refs.togglerMenu.addEventListener("click", toggleMenu);
+
+	refs.openModal.addEventListener("click", toggleModal);
+	refs.closeModal.addEventListener("click", toggleModal);
 
 	function toggleMenu() {
 		const expanded = refs.togglerMenu.getAttribute("aria-expanded") === "true" || false;
@@ -22,5 +30,11 @@
 			toggleMenu()
 		};
 		
+	}
+	function toggleModal() {
+		closeMenu()
+		refs.modal.classList.toggle("is-hidden");
+		refs.backDrop.classList.toggle("is-hidden");
+		document.body.classList.toggle("off_overflow");
 	}
 })();
