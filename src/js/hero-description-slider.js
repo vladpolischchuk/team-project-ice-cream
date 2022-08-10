c7bce36cd20119534b19805d9bbc2d4b5504bf55
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-08-10 14:55:28 +0300

    Update hero-description-slider.js

--- src/js/hero-description-slider.js
+++ src/js/hero-description-slider.js
@@ -1,15 +1,22 @@
 (() => {
 	const refs = {
 		togglerDescription: document.querySelector("[data-description-toggle]"),
-		descriptionSlides: document.querySelectorAll("[data-description-slides]"),
+		descriptionSlides: document.querySelector("[data-description-slides]"),
 	};
 
 	refs.togglerDescription.addEventListener("click", toggleDescription);
 
 	function toggleDescription() {
-		if (refs.togglerMenu.classList.contains("is-open")){
-			toggleMenu();
-		};
+		if (refs.descriptionSlides.classList.contains("show-first")){
+			refs.descriptionSlides.classList.toggle("show-first");
+			refs.descriptionSlides.classList.toggle("show-two");
+		} else if(refs.descriptionSlides.classList.contains("show-two")) {
+			refs.descriptionSlides.classList.toggle("show-three");
+			refs.descriptionSlides.classList.toggle("show-two");
+		} else {
+			refs.descriptionSlides.classList.toggle("show-first");
+			refs.descriptionSlides.classList.toggle("show-three");
+		}
 		
 	}
 
